880794e48dfce33dbaed466584609204d4c74386
agus grandon <agustina107.grandon@gmail.com>
2025-05-30 16:07:18 -0300

    inicie el menu

--- src/parcial/Parcial.java
+++ src/parcial/Parcial.java
@@ -1,4 +1,5 @@
 package parcial;
+import java.util.Scanner;
 
 /**
  *
@@ -6,11 +7,37 @@ package parcial;
  */
 public class Parcial {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        boolean seguir = true;
+        
+        while(seguir){
+            System.out.println("\n======== Menu de opciones ==========");
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            System.out.println("4) Salir");
+            System.out.println("Seleccione una opción: ");
+            
+            opcion = sc.nextInt(); 
+            
+            switch(opcion){
+                case 1:
+                    System.out.println("opcion 1");
+                case 2:
+                    System.out.println("opcion 2");
+                case 3:
+                    System.out.println("opcion 3");
+                case 4:
+                    seguir = false;
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+        
+        sc.close(); 
     }
     
 }
