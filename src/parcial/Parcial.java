f21a1feea7929d152415c78f58f9d2b56773f320
agus grandon <agustina107.grandon@gmail.com>
2025-05-30 15:59:58 -0300

    primer commit

---
+++ src/parcial/Parcial.java
@@ -0,0 +1,16 @@
+package parcial;
+
+/**
+ *
+ * @author agus grandon
+ */
+public class Parcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
